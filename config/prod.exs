55926f9af9e56fd93fc15acf4a40195e5a3a25d1
Jacob Luetzow <jacob@perfectform.io>
2024-01-28 20:22:01 -0700

    added gigalixir config

--- config/prod.exs
+++ config/prod.exs
@@ -19,16 +19,10 @@ config :logger, level: :info
 # To get SSL working, you will need to add the `https` key
 # to the previous section and set your `:url` port to 443:
 #
-#     config :carafe, CarafeWeb.Endpoint,
-#       ...,
-#       url: [host: "example.com", port: 443],
-#       https: [
-#         ...,
-#         port: 443,
-#         cipher_suite: :strong,
-#         keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-#         certfile: System.get_env("SOME_APP_SSL_CERT_PATH")
-#       ]
+config :carafe, CarafeWeb.Endpoint,
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [host: "example.com", port: 80]
+
 #
 # The `cipher_suite` is set to `:strong` to support only the
 # latest and more secure SSL ciphers. This means old browsers
--- config/runtime.exs
+++ config/runtime.exs
@@ -31,7 +31,7 @@ if config_env() == :prod do
   maybe_ipv6 = if System.get_env("ECTO_IPV6"), do: [:inet6], else: []
 
   config :carafe, Carafe.Repo,
-    # ssl: true,
+    ssl: true,
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     socket_options: maybe_ipv6
